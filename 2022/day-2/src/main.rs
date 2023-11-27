7b4886d8beccd25a06bf6bf4245cdcd2663f2d47
JoshuaHenriques <joshuahenriques95@gmail.com>
2023-11-27 09:29:07 -0500

    day 2

--- 2022/day-2/src/main.rs
+++ 2022/day-2/src/main.rs
@@ -2,7 +2,6 @@ use std::{
     // cmp,
     fs::File,
     io::{prelude::*, BufReader}, 
-    collections::HashMap,
 };
 
 fn main() {
@@ -12,18 +11,50 @@ fn main() {
         .map(|line| line.expect("Could not parse line!"))
         .collect();
 
-    let mut shapes = HashMap::new();
-    shapes.insert("X".to_string(), 1);
-    shapes.insert("Y".to_string(), 2);
-    shapes.insert("Z".to_string(), 3);
-
     let mut total_score: u32 = 0;
-    // let mut curr_score: u32 = 0;
     for (i, line) in lines.iter().enumerate() {
         let results: Vec<&str> = line.split(" ").collect();
         let opp = results[0];
         let me = results[1];
-        println!("opp: {:?}, me: {:?}", opp, me);
-        // match line
+        // println!("opp: {:?}, me: {:?}", opp, me);
+        
+        match opp {
+            "A" => {
+                match me {
+                    "X" => total_score += 3,
+                    "Y" => total_score += 1 + 3,
+                    "Z" => total_score += 2 + 6,
+                    // "X" => total_score += 1 + 3,
+                    // "Y" => total_score += 2 + 6,
+                    // "Z" => total_score += 3,
+                    _ => (),
+                }
+            },
+            "B" => {
+                match me {
+                    "X" => total_score += 1,
+                    "Y" => total_score += 2 + 3,
+                    "Z" => total_score += 3 + 6,
+                    // "X" => total_score += 1,
+                    // "Y" => total_score += 2 + 3,
+                    // "Z" => total_score += 3 + 6,
+                    _ => (),
+                }
+            },
+            "C" => {
+                match me {
+                    "X" => total_score += 2,
+                    "Y" => total_score += 3 + 3,
+                    "Z" => total_score += 1 + 6,
+                    // "X" => total_score += 1 + 6,
+                    // "Y" => total_score += 2,
+                    // "Z" => total_score += 3 + 3,
+                    _ => (),
+                }
+            },
+            _ => (),
+        }
     }
+
+    println!("{:?}", total_score);
 }
