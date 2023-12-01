a210d9dc716a01734e95d43decea39794e3dbdb2
JoshuaHenriques <joshuahenriques95@gmail.com>
2023-11-30 23:45:57 -0500

    update

--- 2022/day-3/src/main.rs
+++ 2022/day-3/src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     // cmp,
     fs::File,
+    collections::HashSet,
     io::{prelude::*, BufReader}, 
 };
 
@@ -11,9 +12,26 @@ fn main() {
         .map(|line| line.expect("Could not parse line!"))
         .collect();
 
-    let mut sum_priorities: u32 = 0;
-    for (i, line) in lines.iter().enumerate() {
+    let mut sum_priorities: i32 = 0;
+    let mut priorities = HashSet::<char>::new();
+    for (_, line) in lines.iter().enumerate() {
         let (left, right) = line.split_at(line.len() / 2);
-        println!("left: {}\nright: {}\noriginal: {}", left, right, line);
+        // println!("left: {}\nright: {}\noriginal: {}", left, right, line);
+
+        for char in left.chars() {
+            priorities.insert(char);
+        }
+        
+        for char in right.chars() {
+            if priorities.contains(&char) {
+                let a = 'a' as i32;
+                let char_num = char as i32;
+                println!("{}: {}", char_num, a); 
+                sum_priorities += char_num - a;
+                break;
+            }
+        }
     }    
+
+    println!("{}", sum_priorities);
 }
